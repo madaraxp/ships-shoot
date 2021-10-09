3da7a97ee4a9887d707ca8504832161574f78b1c
William da Silva Dias <wsil7@hotmail.com>
2021-10-09 16:17:34 -0300

    First commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    """Class for settings"""
+    def __init__(self):
+        self.screen_width = 1180
+        self.screen_height = 900
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        
+        #Bullets settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
